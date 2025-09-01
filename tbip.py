3647c3b85c195c1a63719ef0391cd909bbd17105
XinyuanYang <97706070+XinyuanYang@users.noreply.github.com>
2025-08-31 23:17:14 -0400

    tbip.py

--- tbip.py
+++ tbip.py
@@ -186,7 +186,7 @@ def build_lognormal_variational_parameters(initial_document_loc,
       initializer=tf.constant(np.log(initial_document_loc)))
   objective_topic_loc = tf.Variable(
       "objective_topic_loc",
-      initializer=tf.constant(np.log(initial_objective_topic_loc)))
+      initializer = np.zeros((num_documents,), dtype=np.float32) 
   document_scale_logit = tf.Variable(
       "document_scale_logit",
       shape=[num_documents, num_topics],
