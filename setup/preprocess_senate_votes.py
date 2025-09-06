951fc4dd140d156f71e146d540a906d010fee78b
XinyuanYang <97706070+XinyuanYang@users.noreply.github.com>
2025-09-06 01:01:04 -0400

    preprocess_senate_votes.py

--- setup/preprocess_senate_votes.py
+++ setup/preprocess_senate_votes.py
@@ -27,8 +27,9 @@ def main(argv):
   del argv
   project_dir = os.path.abspath(
       os.path.join(os.path.dirname(__file__), os.pardir)) 
-  source_dir = os.path.join(project_dir, "data".format(FLAGS.senate_session))
-  data_dir = os.path.join(source_dir) 
+  source_dir = os.path.join(
+      project_dir, "data/senate-votes/{}".format(FLAGS.senate_session))
+  data_dir = os.path.join(source_dir, "raw")
   save_dir = os.path.join(source_dir, "clean")
 
   votes_df = pd.read_csv(
